4836b09e689aa121e53a7948c61dd3905e4d4dbe
vivien leroux <vivienmichelesi@gmail.com>
2020-11-19 23:39:21 +0100

    Ajout Header et Footer partout sauf Corentin, Tri dans les oeuvres, Header cliquable pour home

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Header.css';
 
 const Header = () => {
   return (
     <div className="Header">
-      <h1>Uber Arts</h1>
-      <h2>Let art travel to you</h2>
+      <Link to="/" className="HeaderLink">
+        <h1>Uber Arts</h1>
+        <h2>Let art travel to you</h2>
+      </Link>
     </div>
   );
 };
