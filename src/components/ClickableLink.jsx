309c04e6b59158e00e9005d89691245389bd1aef
vivien leroux <vivienmichelesi@gmail.com>
2020-11-20 00:02:30 +0100

    Essai sur les ClickableLink non concluant

---
+++ src/components/ClickableLink.jsx
@@ -0,0 +1,12 @@
+// import React from 'react';
+// // import {Link} from 'react-router-dom';
+
+// Const ClickableLink = () => {
+//   return(
+//     <label type='submit' className='RecherchArtist'>
+//       <placeholder>Name of the Artist</placeholder>
+//     </label>
+//   )
+// }
+
+// export default ClickableLink;
