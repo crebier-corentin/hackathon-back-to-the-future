8c44e16ab8085f92d58bf28c3ff86d1761b0fa11
e-floran <e-floran@hotmail.fr>
2020-11-19 12:38:34 +0100

    created first part of return and state

--- src/pages/Order.jsx
+++ src/pages/Order.jsx
@@ -1,19 +1,34 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { useParams } from 'react-router-dom';
 
-class Order extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      name: 'Order',
-    };
-  }
-
-  render() {
-    const { name } = this.state;
-
-    return <h1>{name}</h1>;
-  }
+function Order() {
+  // const [premium, setPremium] = useState(false);
+  const { name } = useParams();
+  return (
+    <div className="orderContainer">
+      <h2>Order a masterpiece from {name}</h2>
+      <section>
+        <article className="orderLord">
+          <h4>Choose lord service</h4>
+          <p>
+            By chosing our lord service, you have the ability to send a picture
+            to the artist. Based on this, our errand boy will bring the
+            masterpiece from {name} to you within a few minutes.
+          </p>
+        </article>
+        <article className="orderKing">
+          <h4>Choose king service</h4>
+          <p>
+            By chosing our king service, a complete Uber Arts team will bring
+            the artist directly to you. You will have the possibility to talk
+            with {name}, allowing this master to have a better feel of your
+            royal desire. A wide selection of clothes and accessories will also
+            be brought to you by our team to suit your needs.
+          </p>
+        </article>
+      </section>
+    </div>
+  );
 }
 
 export default Order;
