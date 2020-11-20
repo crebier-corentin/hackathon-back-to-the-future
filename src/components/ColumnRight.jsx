8fb1ef410fd31f2299bf1e850faca12b01efaa1b
Corentin Crébier <crebier.corentin@gmail.com>
2020-11-20 09:25:02 +0100

    Fix Column z-index

--- src/components/ColumnRight.jsx
+++ src/components/ColumnRight.jsx
@@ -2,16 +2,14 @@ import React from 'react';
 import './ColumnRight.css';
 
 const ColumnRight = () => (
-  <div className="invert">
-    <div className="columnRight">
-      <div className="groove" />
-      <div className="groove" />
-      <div className="groove" />
-      <div className="groove" />
-      <div className="groove" />
-      <div className="head" />
-      <div className="head2" />
-    </div>
+  <div className="columnRight">
+    <div className="groove" />
+    <div className="groove" />
+    <div className="groove" />
+    <div className="groove" />
+    <div className="groove" />
+    <div className="head" />
+    <div className="head2" />
   </div>
 );
 
